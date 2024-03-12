eb9e0dbf4becf632da1863ad84c9e75976372eba
mtk2608 <kamaldientayieb@gmail.com>
2024-03-12 23:36:03 +0200

    Start of project

---
+++ HoodGrocer/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HoodGrocer
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HoodGrocer/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace HoodGrocer
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
