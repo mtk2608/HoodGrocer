c644236fc65a4144253686c054ba8ec1f53d10c7
mtk2608 <kamaldientayieb@gmail.com>
2024-03-15 00:47:54 +0200

    Button Created

--- HoodGrocer/UI/HomePage.xaml.cs
+++ HoodGrocer/UI/HomePage.xaml.cs
@@ -5,5 +5,18 @@ public partial class HomePage : ContentPage
 	public HomePage()
 	{
 		InitializeComponent();
-	}
+        BindingContext = new YourViewModel();
+    }
+
+    public class YourViewModel
+    {
+        public ImageSource ImageSource { get; } = ImageSource.FromFile("Images/saskowhitebread.png");
+    }
+
+    private async void Add_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new CartPage());
+    }
+
+
 }
