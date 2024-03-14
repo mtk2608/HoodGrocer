d16a4250e16b6cc9c241d3f0076b6fcacd8512b6
mtk2608 <kamaldientayieb@gmail.com>
2024-03-14 04:50:16 +0200

    Changes

--- HoodGrocer/Services/HoodGrocerLocalDatabase.cs
+++ HoodGrocer/Services/HoodGrocerLocalDatabase.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
+using SQLiteNetExtensions.Extensions;
+using SQLitePCL;
 
 namespace HoodGrocer.Services
 {
-    internal class HoodGrocerLocalDatabase
+    public class HoodGrocerLocalDatabase
     {
+        private SQLiteConnection _dbConnection;
+
+        public string GetDatabasePath()
+        {
+            string filename = "hoodGrocer.db";
+            string pathToDb = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+            return Path.Combine(pathToDb, filename);
+        }
+
+        public HoodGrocerLocalDatabase()
+        {
+            _dbConnection = new SQLiteConnection(GetDatabasePath());
+
+
+        }
     }
 }
